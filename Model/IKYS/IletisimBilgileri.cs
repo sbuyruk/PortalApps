280474cf6f3ce199955b2b0ce4f612962f5769c5
asbuyrukW11 <sbuyruk@gmail.com>
2026-09-18 13:18:50 +0300

    IKYS insert sorgularını parametreli hale getir - ikinci paket

--- Model/IKYS/IletisimBilgileri.cs
+++ Model/IKYS/IletisimBilgileri.cs
@@ -1,4 +1,5 @@
 
+using DAO.Ortak;
 using Model.Ortak;
 using System;
 using System.Collections.Generic;
@@ -43,8 +44,8 @@ namespace Model.IKYS
                 GenericEntity<IletisimBilgileri> genericEntity = new GenericEntity<IletisimBilgileri>(ProjeConstants.SQL_INSERT);
                 OlusturmaTarihi = DateTime.Now;
                 Olusturan = UtilityHelper.GetCurrentUserName();
-                string sqlString = genericEntity.GetQuery(this);
-                int id = dao.Insert(sqlString);
+                SqlQuery query = genericEntity.GetQueryParametreli(this);
+                int id = dao.Insert(query);
                 if (id > 0 && ProjeConstants.IKYS_SAVE_LOG)
                 {
                     OlayKayit olayKayit = new OlayKayit();
