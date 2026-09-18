916b65d3ab6b87a844be3dd304d0af94ca4e2b49
asbuyrukW11 <sbuyruk@gmail.com>
2026-09-18 10:58:19 +0300

    IKYS tanım insert sorgularını parametreli hale getir

--- Model/IKYS/IzinTanim.cs
+++ Model/IKYS/IzinTanim.cs
@@ -1,4 +1,5 @@
 
+using DAO.Ortak;
 using Model.Ortak;
 using System;
 using System.Collections.Generic;
@@ -29,8 +30,8 @@ namespace Model.IKYS
                 GenericEntity<IzinTanim> genericEntity = new GenericEntity<IzinTanim>(ProjeConstants.SQL_INSERT);
                 OlusturmaTarihi = DateTime.Now;
                 Olusturan = UtilityHelper.GetCurrentUserName();
-                string sqlString = genericEntity.GetQuery(this);
-                int id = dao.Insert(sqlString);
+                SqlQuery query = genericEntity.GetQueryParametreli(this);
+                int id = dao.Insert(query);
                 if (id > 0 && ProjeConstants.IKYS_SAVE_LOG)
                 {
                     OlayKayit olayKayit = new OlayKayit();
--- Model/IKYS/UnvanTanim.cs
+++ Model/IKYS/UnvanTanim.cs
@@ -1,4 +1,5 @@
 
+using DAO.Ortak;
 using Model.Ortak;
 using System;
 using System.Collections.Generic;
@@ -33,8 +34,8 @@ namespace Model.IKYS
                 GenericEntity<UnvanTanim> genericEntity = new GenericEntity<UnvanTanim>(ProjeConstants.SQL_INSERT);
                 OlusturmaTarihi = DateTime.Now;
                 Olusturan = UtilityHelper.GetCurrentUserName();
-                string sqlString = genericEntity.GetQuery(this);
-                int id = dao.Insert(sqlString);
+                SqlQuery query = genericEntity.GetQueryParametreli(this);
+                int id = dao.Insert(query);
                 if (id > 0 && ProjeConstants.IKYS_SAVE_LOG)
                 {
                     OlayKayit olayKayit = new OlayKayit();
