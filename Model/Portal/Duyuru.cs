dd4a52e9cb342660b5538179e7a05d86fc6740a2
asbuyrukW11 <sbuyruk@gmail.com>
2026-09-19 23:47:32 +0300

    Portal ve Ortak delete işlemlerini audit loglu parametreli hale getir

--- Model/Portal/Duyuru.cs
+++ Model/Portal/Duyuru.cs
@@ -160,8 +160,8 @@ namespace Model.Portal
                 {
                     GenericEntity<Duyuru> genericEntity = new GenericEntity<Duyuru>(ProjeConstants.SQL_UPDATE);
                     DegistirmeTarihi = DateTime.Now;
-                    string sqlString = genericEntity.GetQuery(this);
-                    isSuccess = dao.Update2Db(sqlString);
+                    SqlQuery query = genericEntity.GetQueryParametreli(this);
+                    isSuccess = dao.Update2Db(query);
                 }
             }
             catch (Exception)
@@ -172,11 +172,10 @@ namespace Model.Portal
         }
         public override bool Delete()
         {
-            string sqlString = string.Format(@"DELETE 
-                               FROM Duyuru_Table
-                               WHERE Id={0}", Id);
+            GenericEntity<Duyuru> genericEntity = new GenericEntity<Duyuru>(ProjeConstants.SQL_DELETE);
+            SqlQuery query = genericEntity.GetQueryParametreli(this);
 
-            bool isSuccess = dao.DeleteFromDb(sqlString, this);
+            bool isSuccess = dao.DeleteFromDb(query, this);
 
             return isSuccess;
         }
